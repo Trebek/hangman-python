3051767f38425744cf03c70d4cdbf703043e7f9a
Alex <Trebek@users.noreply.github.com>
2013-12-29 16:45:18 -0800

    Update hangman.py
    Rearranged some stuff, and fixed a couple of small things. Vague changelog, I know.

--- hangman.py
+++ hangman.py
@@ -1,8 +1,8 @@
 #===============================================================================
 # Hangman, in Python
 #-------------------------------------------------------------------------------
-# Version: 0.1.7
-# Updated: 25-11-2013
+# Version: 0.1.8
+# Updated: 29-12-2013
 # Author: Alex Crawford
 # License: MIT
 #===============================================================================
@@ -101,89 +101,61 @@ class Game(object):
     
     alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    wordlist = [
-    "Cup", 
-    "Word",
-    "Test",
-    "Hangman",
-    "Python",
-    "Snake",
-    "Green",
-    "Red",
-    "Awesome",
-    "Chair",
-    "Human",
-    "Machine",
-    "Computer",
-    "Keyboard",
-    "Cat",
-    "Dog",
-    "Hand",
-    "Foot",
-    "Left",
-    "Right"
-    ]
-
-    easywords = []
-    normwords = []
-    hardwords = []
-    worddict = {
-        1: easywords,
-        2: normwords,
-        3: hardwords
-    }
-
-    wordlvl = 1
-    theword = None
-    hidden = []
-
-    guess = None
-    gletts = []
-    gnum = 0
-    left = 6
-    men = 3
-
-    score = 0
-    totalscore = 0
-
-    running = True
-    quitting = False
-    quitmsg = "I hope you enjoyed playing! Good bye.\n"
-
-    exwords = ["EXIT", "QUIT", "END"]
-
     def __init__(self):
-        """Game initialization method"""
+        """Game initialization method."""
 
-        self.title("HANGMAN, IN PYTHON", "A Game by Alex Crawford\nv0.1.6")
+        self.wordlist = [
+            "Cup", 
+            "Word",
+            "Test",
+            "Hangman",
+            "Python",
+            "Snake",
+            "Green",
+            "Red",
+            "Awesome",
+            "Chair",
+            "Human",
+            "Machine",
+            "Computer",
+            "Keyboard",
+            "Cat",
+            "Dog",
+            "Hand",
+            "Foot",
+            "Left",
+            "Right"
+        ]
 
-        cmd = raw_input("Difficulty level? (1-3) ")
+        self.easywords = []
+        self.normwords = []
+        self.hardwords = []
 
-        if cmd == "1":
-            self.men = 5
-        elif cmd == "2":
-            self.men = 3
-        elif cmd == "3":
-            self.men = 1
-        else:
-            pass
-        
-        cmd = raw_input("Load the word list from a file? (y/n) ")
-        
-        if cmd in ['Y', 'y']:
-            fromfile = True
-            self.getwordfile()
-            print "\nWord file loaded."
-        else:
-            print "\nNo word file loaded."
+        self.wordhash = {
+            1: self.easywords,
+            2: self.normwords,
+            3: self.hardwords
+        }
 
-        print "\nEnter 'end', exit', or 'quit' as a guess to quit."
-        
-        self.sortwords()
+        self.theword = None
+        self.hidden = []
 
-        self.figurescore()
+        self.guess = None
+        self.gletts = []
+        self.gnum = 0
+        self.gleft = 6
 
-        del Game.wordlist
+        self.wordlvl = 1
+        self.score = 0
+        self.totalscore = 0
+        self.men = 3
+
+        self.running = True
+        self.quitting = False
+
+        self.quitmsg = "\nI hope you enjoyed playing! Good bye.\n"
+
+        self.exwords = ["exit", "quit", "end"]
 
     def checkguess(self):
         """Checks the player's guess for a correct letter."""
@@ -196,7 +168,7 @@ class Game(object):
             self.gletts.append(self.guess[0].upper())
         else:
             self.gnum += 1
-            self.left -= 1
+            self.gleft -= 1
             return
 
         for c in self.theword:
@@ -212,11 +184,11 @@ class Game(object):
         if right > 0:
             print "\nGood guess! Don't let it go to your head though."
             
-        if ((wrong == len(self.theword) or right == 0) and             
-                self.guess.upper() not in self.exwords):
+        if (wrong == len(self.theword) or right == 0 and             
+                self.guess.lower() not in self.exwords):
             self.gnum += 1
-            self.left -= 1
-            if self.left < 5 and self.score > 0:
+            self.gleft -= 1
+            if self.gleft < 5 and self.score > 0:
                 self.score -= 1
             print "\nIncorrect. No one can be right all of the time."
 
@@ -225,9 +197,12 @@ class Game(object):
             
         print NL + HR + NL
 
-    def checkwords(self):
-        """Checks the level the player is on, and acts depending on that."""
-        if len(self.worddict[self.wordlvl]) > 0:
+    def checklvl(self):
+        """Checks the level the player is on. If the player has solved
+        all of the words in that level, the player is moved up a level.
+
+        """
+        if len(self.wordhash[self.wordlvl]) > 0:
             if self.wordlvl == 1:
                 self.easywords.remove(self.theword)
                 if len(self.easywords) == 0 and len(self.normwords) > 0:
@@ -242,17 +217,16 @@ class Game(object):
                 self.hardwords.remove(self.theword)
 
     def display(self, win=False):
-        """Prints the current hangman art, and other relevant information."""
-        
+        """Prints the current hangman art, and other relavant information."""
+
         if not win:
             print "Level: " + str(self.wordlvl)
             print "Score: " + str(self.score)
             print "Men left: " + str(self.men - 1)
             print asciihang[self.gnum]
             print " ".join(self.hidden) + NL
-            # print "Guesses: " + " ".join(tempguessed)
             print "Guesses: " + " ".join(sorted(self.gletts))
-            print "Guesses left: " + str(self.left) + NL
+            print "Guesses left: " + str(self.gleft) + NL
         else:
             print "".join(self.hidden).upper() + NL
 
@@ -269,9 +243,9 @@ class Game(object):
             self.guess = raw_input("What's your guess? ")
             if self.guess in self.alpha and self.guess is not "":
                 break
-            elif self.guess.upper() in self.exwords:
+            elif self.guess.lower() in self.exwords:
                 self.quitting = True
-                return
+                break
             else:
                 print "\nInvalid guess. Must be a letter.\n"
 
@@ -280,28 +254,27 @@ class Game(object):
         as the current word.
 
         """
-        while True:
-            if len(self.worddict[self.wordlvl]) > 0:
-                self.theword = choice(self.worddict[self.wordlvl])
-                break
-            else:
-                break
+        if len(self.wordhash[self.wordlvl]) > 0:
+            self.theword = choice(self.wordhash[self.wordlvl])
+        else:
+            print "\nOops! No words to load. Exiting.\n"
+            exit()
 
     def getwordfile(self):
         """Opens a file named 'words.txt', if it's present, and uses
         that for the word list.
 
         """
-        self.wordlist = []
-
         wordtxt = open("words.txt", "r")
+
+        self.wordlist = []
         
         for line in wordtxt:
             self.wordlist.append(line.rstrip(NL))
         
         wordtxt.close()
 
-    def hideword(self, guess=None):
+    def hideword(self):
         """Generate a hidden version of the chosen word."""
         
         temp = []
@@ -311,11 +284,50 @@ class Game(object):
                         
         self.hidden = temp
 
-    def loop(self):
+    def intro(self):
+        """Prints the title, and gets some input from the player,
+        including difficulty level and whether or not to load the
+        wordlist from a file.
+
+        """
+
+        self.title("HANGMAN, IN PYTHON", "A Game by Alex Crawford", "v0.1.8")
+
+        cmd = raw_input("Difficulty level? (1-3) ")
+
+        if cmd == "1":
+            self.men = 5
+        elif cmd == "2":
+            self.men = 3
+        elif cmd == "3":
+            self.men = 1
+        else:
+            pass
+        
+        cmd = raw_input("Load the word list from a file? (y/n) ")
+        
+        if cmd in ['Y', 'y']:
+            try:
+                self.getwordfile()
+                print "\nWord file loaded."
+            except:
+                print "\nCouldn't load file. Loading default."
+                pass
+        else:
+            print "\nDefault word file loaded."
+
+        print "\nEnter 'end', exit', or 'quit' as a guess to quit."
+
+    def mainloop(self):
         """The main game loop."""
 
         playing = True
-        fromfile = False
+
+        self.intro()
+        self.figurescore()
+        self.sortwords()
+
+        # del self.wordlist
 
         print NL + HR + NL
 
@@ -328,21 +340,22 @@ class Game(object):
                 
                 self.display()
                 self.getguess()
-                self.checkguess()
-                
+
                 if self.quitting:
                     self.running = False
+                    print NL + HR
                     print self.quitmsg
                     break
+
+                self.checkguess()
                 
-                # if self.gnum == len(asciihang) - 1:
-                if self.left == 0:
+                if self.gleft == 0:
                     self.display()
                     print "You've run out of guesses... Sorry about that.\n"
                     print "The word was: " + self.theword.upper() + NL
                     self.men -= 1
                     self.reset()
-                    self.checkwords()
+                    self.checklvl()
                     print HR + NL
                     break
                 elif "".join(self.hidden) == self.theword:
@@ -350,17 +363,17 @@ class Game(object):
                     print "You solved the word! Exciting, I know."
                     print "+10 points\n"
                     self.score += 10
-                    if self.left == 6:
+                    if self.gleft == 6:
                         print "You didn't make a single wrong guess!"
                         print "+10 bonus points\n"
                         self.score += 10
-                    self.checkwords()
+                    self.checklvl()
                     self.reset()
                     print HR + NL
                     break
 
             self.scorestr = "{0} {1} {2} {3}{4}".format("Final score:", 
-                self.score, "/", self.totalscore, NL)
+                    self.score, "/", self.totalscore, NL)
 
             if self.men == 0:
                 print "All of your men have been hanged!"
@@ -382,22 +395,24 @@ class Game(object):
 
         self.gletts = []
         self.gnum = 0
-        self.left = 6
+        self.gleft = 6
 
     def sortwords(self):
         """Sorts the words in the main wordlist into separate lists,
-        depending on their length
+        for each level, depending on their length. It also deletes
+        self.wordlist once the words have been sorted.
 
         """
         for word in self.wordlist:
-            if len(word) in range(1, 4):
+            if len(word) in range(1, 5):
                 self.easywords.append(word)
-            elif len(word) in range(5, 6):
+            elif len(word) in range(5, 7):
                 self.normwords.append(word)
             else:
                 self.hardwords.append(word)
+        del self.wordlist
 
-    def title(self, msg, sub=None, nl=True):
+    def title(self, msg, sub=None, ver=None, nl=True):
         """Prints a title, with horizontal rules above and below it. 
         Also prints a subtitle below the title, if provided.
 
@@ -406,6 +421,8 @@ class Game(object):
         print msg
         if sub is not None:
             print sub
+        if ver is not None:
+            print ver
         print HR
         if nl:
             print
@@ -415,6 +432,5 @@ class Game(object):
 #===============================================================================
 
 if __name__ == '__main__':
-
     game = Game()
-    game.loop()
+    game.mainloop()
